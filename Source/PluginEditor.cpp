543cb485680f52276f71f4b4f3888a8b9fe07912
ari <ari@llamanet.de>
2020-12-21 16:58:50 +0100

    Remove old onValuechange listeners
    Also removed obsolete #define _USE_MATH_DEFINES

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,5 +1,3 @@
-#define _USE_MATH_DEFINES
-
 #include <cmath>
 
 #include "PluginProcessor.h"
@@ -21,8 +19,7 @@ AmpliModAudioProcessorEditor::AmpliModAudioProcessorEditor (AmpliModAudioProcess
     addAndMakeVisible(ampLabel);
     ampLabel.setText("Mix", juce::dontSendNotification);
     ampLabel.attachToComponent(&ampSlider, true);
-    ampSlider.onValueChange = [this] { audioProcessor.sin_amp = ampSlider.getValue() / 100; };
-    frequencySlider.onValueChange = [this] { audioProcessor.phase_delta = 2 * M_PI * frequencySlider.getValue() / audioProcessor.fs; DBG(2 * M_PI * frequencySlider.getValue() / audioProcessor.fs); };
+
     frequencySlider.setColour(juce::Slider::ColourIds::backgroundColourId, juce::Colour::fromRGB(255, 170, 220));
     frequencySlider.setColour(juce::Slider::ColourIds::trackColourId, juce::Colour::fromRGB(240, 140, 255));
     frequencySlider.setColour(juce::Slider::ColourIds::thumbColourId, juce::Colour::fromRGB(220, 120, 240));
@@ -38,6 +35,7 @@ AmpliModAudioProcessorEditor::AmpliModAudioProcessorEditor (AmpliModAudioProcess
     ampSlider.setColour(juce::Slider::ColourIds::textBoxHighlightColourId, juce::Colour::fromRGB(220, 120, 240));
     ampSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colour::fromRGB(90, 10, 90));
     ampLabel.setColour(juce::Label::ColourIds::textColourId, juce::Colour::fromRGB(90, 10, 90));
+    
 }
 
 AmpliModAudioProcessorEditor::~AmpliModAudioProcessorEditor()
@@ -47,7 +45,6 @@ AmpliModAudioProcessorEditor::~AmpliModAudioProcessorEditor()
 void AmpliModAudioProcessorEditor::paint (juce::Graphics& g)
 {
     g.fillAll (juce::Colour::fromRGB(255,200,230));
-
     g.setColour (juce::Colour::fromRGB(90, 10, 90));
     g.setFont (15.0f);
     g.drawFittedText ("AmpMod", getLocalBounds(), juce::Justification::topLeft, 1);
