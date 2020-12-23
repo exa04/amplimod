7c81a24b6fcd37109dbb3a47f7b7b1cc5db180a9
ari <ari@llamanet.de>
2020-12-23 16:21:14 +0100

    Add files via upload

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -11,7 +11,6 @@ juce::String AmpliModAudioProcessor::paramFreq("Frequency");
 juce::String AmpliModAudioProcessor::paramMix("Mix");
 juce::String AmpliModAudioProcessor::paramStereoOffset("Stereo Offset");
 
-//==============================================================================
 AmpliModAudioProcessor::AmpliModAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -55,132 +54,30 @@ AmpliModAudioProcessor::AmpliModAudioProcessor()
     mState.addParameterListener(paramStereoOffset, this);
 }
 
-void AmpliModAudioProcessor::parameterChanged(const juce::String& parameterID, float newValue)
+void AmpliModAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    if (parameterID == paramMix) {
-        mMix = newValue;
-    }
-    else if (parameterID == paramFreq) {
-        mFreq = newValue;
-    }
-    else if (parameterID == paramStereoOffset) {
-        mStereoOffset = newValue;
-    }
-}
-
-AmpliModAudioProcessor::~AmpliModAudioProcessor()
-{
-}
-
-//==============================================================================
-const juce::String AmpliModAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
-
-bool AmpliModAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool AmpliModAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool AmpliModAudioProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-double AmpliModAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int AmpliModAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int AmpliModAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void AmpliModAudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const juce::String AmpliModAudioProcessor::getProgramName (int index)
-{
-    return {};
-}
-
-void AmpliModAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
-
-//==============================================================================
-void AmpliModAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
     fs = sampleRate;
     phase_delta = 2 * M_PI / fs;
 }
 
-void AmpliModAudioProcessor::releaseResources()
+void AmpliModAudioProcessor::parameterChanged(const juce::String& parameterID, float newValue)
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+    if (parameterID == paramMix) mMix = newValue;
+    else if (parameterID == paramFreq) mFreq = newValue;
+    else if (parameterID == paramStereoOffset) mStereoOffset = newValue;
 }
-
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool AmpliModAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-#endif
 
 void AmpliModAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     double sin_amp = mMix.get() / 100;
     phase_delta = 2 * M_PI * mFreq.get() / fs;
     float phase_offset = 2 * M_PI * mStereoOffset.get() / 100;
+
     juce::ScopedNoDenormals noDenormals;
+
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
+
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
@@ -188,7 +85,9 @@ void AmpliModAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     {
         auto* channelData = buffer.getWritePointer (channel);
         for (int i = 0; i < buffer.getNumSamples(); ++i){
-            if (phase_index[channel] > 2 * M_PI) { phase_index[channel] = 0; };
+            if (phase_index[channel] > 2 * M_PI) {
+                phase_index[channel] = 0;
+            };
             phase_index[channel] += phase_delta;
             float sine;
 
@@ -200,14 +99,35 @@ void AmpliModAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
             channelData[i] *= 1 - (sine * sin_amp);
         }
         DBG(sin_amp);
-     
     }
 }
 
-//==============================================================================
+#if true
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool AmpliModAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+{
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
+    return true;
+#else
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+#if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+#endif
+
+    return true;
+#endif
+}
+#endif
+
 bool AmpliModAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* AmpliModAudioProcessor::createEditor()
@@ -215,7 +135,6 @@ juce::AudioProcessorEditor* AmpliModAudioProcessor::createEditor()
     return new AmpliModAudioProcessorEditor (*this);
 }
 
-//==============================================================================
 void AmpliModAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     juce::MemoryOutputStream stream(destData, false);
@@ -235,9 +154,77 @@ void AmpliModAudioProcessor::setStateInformation (const void* data, int sizeInBy
     }
 }
 
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new AmpliModAudioProcessor();
 }
+
+AmpliModAudioProcessor::~AmpliModAudioProcessor()
+{
+}
+
+const juce::String AmpliModAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool AmpliModAudioProcessor::acceptsMidi() const
+{
+#if JucePlugin_WantsMidiInput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool AmpliModAudioProcessor::producesMidi() const
+{
+#if JucePlugin_ProducesMidiOutput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool AmpliModAudioProcessor::isMidiEffect() const
+{
+#if JucePlugin_IsMidiEffect
+    return true;
+#else
+    return false;
+#endif
+}
+
+double AmpliModAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int AmpliModAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int AmpliModAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void AmpliModAudioProcessor::setCurrentProgram(int index)
+{
+}
+
+const juce::String AmpliModAudioProcessor::getProgramName(int index)
+{
+    return {};
+}
+
+void AmpliModAudioProcessor::changeProgramName(int index, const juce::String& newName)
+{
+}
+
+void AmpliModAudioProcessor::releaseResources()
+{
+}
+
+#endif
